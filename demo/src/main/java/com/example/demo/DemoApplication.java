03421c8c2aad0a99726721dfa738bc4a84350015
1004798 <kyuil.cho@sk.com>
2022-07-04 17:53:02 +0900

    bbb

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 	System.out.println("test789000");
+	System.out.println("abc");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
