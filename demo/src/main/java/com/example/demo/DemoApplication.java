6447d3c784d1f246e602faaf7af7af51e058a7e0
1004798 <kyuil.cho@sk.com>
2022-07-04 17:52:35 +0900

    aa

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-	System.out.println("test789");
+	System.out.println("test789000");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
